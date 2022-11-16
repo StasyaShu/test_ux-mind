05b72661a1202e9a6fbbda3b76242e5547b8c271
StasyaShu <stanislava.semenova@gmail.com>
2022-11-16 13:12:25 +0300

    подключение и стилизация времен.селекта

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,8 @@
-import {manageAccordion} from "./accordion";
+import {manageAccordion} from './modules/init-accordion';
+import {initCustomSelect} from './modules/init-custom-select';
 
 
 window.addEventListener('DOMContentLoaded', () => {
   manageAccordion();
+  initCustomSelect();
 })
---
+++ source/js/modules/init-custom-select.js
@@ -0,0 +1,8 @@
+import {CustomSelect} from './custom-select';
+
+const initCustomSelect = () => {
+  const select = new CustomSelect();
+  select.init();
+};
+
+export {initCustomSelect};
